23bc9c27b166f5b21f927764410d8c326e593b2e
Marwen Smida <smida.marwen@gmail.com>
2023-10-12 08:00:15 +0100

    - Connection method with a decorator

--- utils.py
+++ utils.py
@@ -1,23 +1,31 @@
 import os
+
+import numpy as np
 import pandas as pd
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement
+from cql_queries import *
 from functools import wraps
 
-from cassandra.cluster import Cluster
 
-
-def cassandra_connection(ip, keyspace):
+def cassandra_connection(ip):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             cluster = Cluster([ip])
-            session = cluster.connect(keyspace)
+            session = cluster.connect()
             try:
-                return func(session, cluster, *args, **kwargs)
+                return func(session, *args, **kwargs)
             finally:
                 session.shutdown()
                 cluster.shutdown()
+
         return wrapper
+
     return decorator
+
+
 def load_files_list(path):
     files_list = []
     for root, dirs, files in os.walk(path):
@@ -44,7 +52,6 @@ def all_in_one(files_list):
     concat_df = pd.concat(dfs).reset_index(drop=True)
     concat_df.to_csv("event_datafile_new.csv", index=False)
 
+
 # TODO:  Load data from Snowflake
-
-
 
