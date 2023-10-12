d97407c5fbc175e3256f67c6f4ee7c5368fca75e
Marwen Smida <smida.marwen@gmail.com>
2023-10-12 07:52:14 +0100

    - Add Dockerfile and instructions to run it

--- cql_queries.py
+++ cql_queries.py
@@ -1,9 +1,22 @@
-create_table_1 = """CREATE TABLE IF NOT EXISTS songs_by_sessionID (
+create_table_1 = """CREATE TABLE IF NOT EXISTS %s.songs_by_sessionID (
         sessionID INT,
+        itemInSession INT,
         artist TEXT,
         song TEXT,
         length FLOAT,
-        PRIMARY KEY (sessionID)
+        PRIMARY KEY (sessionID, itemInSession)
     );"""
 
+insert_table_1 = '''INSERT INTO %s.songs_by_sessionID(sessionID, itemInSession, artist, song, length) VALUES (?,?,?,?)'''
 
+
+create_table_2 = """CREATE TABLE IF NOT EXISTS %s.info_by_userid (
+        userId INT,
+        sessionID INT,
+        itemInSession INT,
+        artist TEXT,
+        song TEXT,
+        firstName TEXT,
+        lastName TEXT,
+        PRIMARY KEY (userId, sessionID)
+    );"""
