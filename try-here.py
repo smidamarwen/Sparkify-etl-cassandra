018058b17bdeb2e7151d807f981de0a37a973631
Marwen Smida <smida.marwen@gmail.com>
2023-10-12 14:28:28 +0100

    modify gitignore

---
+++ try-here.py
@@ -0,0 +1,22 @@
+from utils import *
+
+#
+# cluster = Cluster(['localhost'])
+# session = cluster.connect()
+# df = load_data(fields=['sessionId', 'artist', 'song', 'length'])
+# query = '''INSERT INTO my_keyspace_test.songs_by_sessionID(sessionID, artist, song, length) VALUES (?,?,?,?)'''
+# preparedStatement = session.prepare(query)
+# df['song'] = df['song'].fillna('Empty Song')
+# df['length'] = df['length'].fillna(0)
+# df['artist'] = df['artist'].fillna('Empty Artist')
+
+#
+# for item in df.iterrows():
+#     session.execute(preparedStatement, (item[1]["sessionId"], item[1]["artist"], item[1]["song"], item[1]["length"]))
+
+
+whole = pd.read_csv(
+        'event_datafile_new.csv')
+# nan_counts = whole.isna().sum()
+df = whole[whole['firstName'] == 'Sylvie']
+create_table_info_by_userid()
